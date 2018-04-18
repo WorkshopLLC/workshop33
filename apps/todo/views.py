48335cbee498dd5e920bd28e3fb80e780d14a83f
dorreDL <dorregaray.anderson@gmail.com>
2018-04-18 08:24:29 -0500

    add: pre workshop

--- apps/todo/views.py
+++ apps/todo/views.py
@@ -9,6 +9,10 @@ class TodoHomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(TodoHomeView, self).get_context_data(**kwargs)
-        todo_all = Todo.objects.all()
+        todo_all = Todo.objects.filter(status='TDO')
+        todo_doing_all = Todo.objects.filter(status='DNG')
+        todo_done_all = Todo.objects.filter(status='DNE')
         context['todo_all'] = todo_all
+        context['todo_doing_all'] = todo_doing_all
+        context['todo_done_all'] = todo_done_all
         return context
