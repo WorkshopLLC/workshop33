8ff34b4818bd0c03dd31c2ba52fc474d81ed63cb
dorreDL <dorregaray.anderson@gmail.com>
2018-04-18 09:05:19 -0500

    update: theme admin

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -13,6 +13,7 @@ env.read_env('./todo_project/env/{}.env'.format(CURRENT_ENV))
 ALLOWED_HOSTS = ['workshop33.herokuapp.com', 'localhost']
 
 INSTALLED_APPS = [
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -33,6 +34,43 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'todo_project.urls'
+
+JET_DEFAULT_THEME = 'default'
+"""
+JET_THEMES = [
+    {
+        'theme': 'default', # theme folder name
+        'color': '#47bac1', # color of the theme's button in user menu
+        'title': 'Default' # theme title
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#a464c4',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    }
+]"""
+
+ADMIN_SITE_HEADER = "WORKSHOP ADMINISTRATION"
 
 TEMPLATES = [
     {
--- todo_project/urls.py
+++ todo_project/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+
+admin.site.site_header = settings.ADMIN_SITE_HEADER
 
 urlpatterns = [
+    url(r'^jet/', include('jet.urls', 'jet')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include(('apps.todo.urls', 'apps.todo'), namespace='todo'))
 ]
